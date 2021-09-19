f1304168e30c7947df557caf43bc0908bcb47cdb
Andre Linces <andrelinces@yahoo.com.br>
2021-09-19 18:28:19 -0300

    Initial Commit

---
+++ FirebaseConfiguracao/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseConfiguracao
+//
+//  Created by Andre Linces on 19/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
